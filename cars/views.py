e20b3dc006f32370023f8788e4d19cfffd06423d
Roberto Moreira <robertodev04@gmail.com>
2025-03-26 09:13:57 -0300

    lidando com a função filter

--- cars/views.py
+++ cars/views.py
@@ -4,7 +4,10 @@ from cars.models import Car
 
 def cars_view(request):
     cars = Car.objects.all()
-    print(cars)
+    # cars = Car.objects.filter(brand=5) # Acessa os campos pelo id na tabela Brand
+    # cars = Car.objects.filter(brand__name= 'Fiat') # Essa é a forma que buscaria pelo name na tabela
+    # cars = Car.objects.filter(model= 'Lancer') # Essa é a forma que buscaria pelo modelo do carro
+    # cars = Car.objects.filter(model__contains= 'Lancer') # Essa é a forma que buscaria pelo modelo do carro não especificamente
 
     return render(
         request, 
